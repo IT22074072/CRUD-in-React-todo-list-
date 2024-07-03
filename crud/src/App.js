009d79d8e9335c94e4b9bc89d496b242728b7a51
IT22074072 <it22074072@my.sliit.lk>
2024-07-03 17:51:55 +0530

    define useStates

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  
   return (
     <div className="App">
-      
+      <h1>
+        <div className='addTask'>
+          <input/>
+          <button>Add Task</button>
+        </div>
+        <div className='list'></div>
+      </h1>
     </div>
   );
 }
