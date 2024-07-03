577912b5391ba07c048b8f0940406e74516e7576
IT22074072 <it22074072@my.sliit.lk>
2024-07-03 19:29:39 +0530

    adding some comments

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,40 +1,33 @@
-import './App.css';
-import {useState} from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
   const [todoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState("");
 
-  const handleChange=(event)=>{
+  const handleChange = (event) => {
     setNewTask(event.target.value);
-
   };
 
-
-  const addTask=()=>{
+  const addTask = () => {
     // can't do this - setTodoList(newTask)
     //...spread operator
-    const newTodoList = [...todoList,newTask];
+    //copy all the elements of the todoList to the newTodoList and append newTask
+    const newTodoList = [...todoList, newTask];
     setTodoList(newTodoList);
-
-
-
   };
-
 
   return (
     <div className="App">
       <h1>
-        <div className='addTask'>
-          <input onChange={handleChange}/>
+        <div className="addTask">
+          <input onChange={handleChange} />
           <button onClick={addTask}>Add Task</button>
         </div>
-        <div className='list'>
-          {todoList.map((task) => {
-            return <h1>{task}</h1>
+        <div className="list">
+          {todoList.map((task) => {  //displaing tasks one by one
+            return <h1>{task}</h1>;
           })}
-
         </div>
       </h1>
     </div>
