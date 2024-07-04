9dd6fd5db0574b5eb4efc8baf09399ced0a829ea
IT22074072 <it22074072@my.sliit.lk>
2024-07-04 15:04:19 +0530

    provide return statement in Task.js

--- crud/src/Task.js
+++ crud/src/Task.js
@@ -1,6 +1,8 @@
 export const Task = (props) => {
-  <div>
-    <h1>{props.taskName}</h1>
-    <button onClick={() => props.deleteTask(props.id)}>X</button>
-  </div>;
+  return (
+    <div>
+      <h1>{props.taskName}</h1>
+      <button onClick={() => props.deleteTask(props.id)}>X</button>
+    </div>
+  );
 };
